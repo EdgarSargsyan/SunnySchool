b69cc0358a36051a048e8fff1975336a4a04c5e1
EdgarSargsyan <sargsyanedg@gmail.com>
2024-03-05 13:05:38 +0000

    files moved

--- key_pair_create.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# creates key pair with name esargsyan_key_pair and type ed25519
-aws ec2 create-key-pair --key-name esargsyan_key_pair --key-type ed25519
-
-echo "Key pair -esargsyan_key_pair- created"
